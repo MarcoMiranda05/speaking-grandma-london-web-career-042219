38336902a96d6a103acf69928181007f1e4439b9
Marco Miranda <marmirand@hotmail.com>
2019-03-29 21:08:18 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -2,8 +2,8 @@ speak_to_grandma = "I LOVE YOU GRANDMA!"
 
 if speak_to_grandma == "I LOVE YOU GRANDMA!"
     puts "I LOVE YOU TOO, PUMPKIN!"
-elsif speak_to_grandma == "I LOVE YOU GRANDMA!"
-    puts "I LOVE YOU TOO, PUMPKIN!"
+elsif speak_to_grandma == "what did you eat today?".upcase
+    puts "NO, NOT SINCE 1938!"
 else
     puts "HUH?! SPEAK UP, SONNY!"
 end
