b58d850e4361cedfaf2f247735c75b85204c8d13
Marco Miranda <marmirand@hotmail.com>
2019-03-29 21:27:24 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -1,6 +1,6 @@
 speak_to_grandma = "I LOVE YOU GRANDMA!"
 if speak_to_grandma == "I LOVE YOU GRANDMA!"
-  puts "I LOVE YOU TOO PUMPKIN!"
+  return "I LOVE YOU TOO PUMPKIN!"
 else
-  puts "wrong"
+  return "wrong"
 end
