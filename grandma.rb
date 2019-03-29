7baf5ea4b1843641359a7886120385c7496fc305
Marco Miranda <marmirand@hotmail.com>
2019-03-29 21:21:19 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -1,7 +1,7 @@
 def speak_to_grandma(string)
   if string != string.upcase
     return "HUH?! SPEAK UP, SONNY!"
-  elsif == "I LOVE YOU GRANDMA!"
+  elsif "I LOVE YOU GRANDMA!"
     return "I LOVE YOU TOO PUMPKIN!"
   elsif string.upcase
     return "NO, NOT SINCE 1938!"
